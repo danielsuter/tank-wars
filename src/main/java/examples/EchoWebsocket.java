4569354867a2931e4995d8778812cae30c52138b
danielsuter <suter_daniel@hotmail.com>
2014-07-18 08:00:38 -0700

    Added some more debugging information. Improved message handling.

--- src/main/java/examples/EchoWebsocket.java
+++ src/main/java/examples/EchoWebsocket.java
@@ -1,10 +1,12 @@
 package examples;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.websocket.OnClose;
+import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
@@ -13,22 +15,49 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint("/echo")
 public class EchoWebsocket {
 
-	private static Set<Session> peers = Collections
-			.synchronizedSet(new HashSet<Session>());
+	private static Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
 
 	@OnMessage
-    public String echo(String message) {
+	public void echo(String message, Session session) {
 		System.out.println("Received messsage from client: " + message);
-        return message + " (from your server)";
-    }
+		try {
+			session.getBasicRemote().sendText(message + " (from your server)");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@OnOpen
 	public void onOpen(Session peer) {
+		System.out.println("opening connection");
 		peers.add(peer);
 	}
 
 	@OnClose
 	public void onClose(Session peer) {
+		System.out.println("closing connection");
 		peers.remove(peer);
+	}
+
+	@OnError
+	public void onError(Session session, Throwable throwable) {
+		if (!isBrokenPipe(throwable)) {
+			throwable.printStackTrace();
+		}
+	}
+
+	/**
+	 * Chrome causes a broken pipe error on connection close, we simply ignore
+	 * this error. As the problem seems to be in the library code.
+	 */
+	private boolean isBrokenPipe(Throwable throwable) {
+		if ("java.io.IOException: Broken pipe".equals(throwable.getMessage())) {
+			System.out.println("Ignoring broken pipe");
+			return true;
+		} else if (throwable.getCause() != null) {
+			return isBrokenPipe(throwable.getCause());
+		} else {
+			return false;
+		}
 	}
 }
