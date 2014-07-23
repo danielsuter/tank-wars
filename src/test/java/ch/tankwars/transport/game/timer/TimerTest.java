53814551a2a1ad7b0a3a0e6d147f199c42ecacd9
danielsuter <suter_daniel@hotmail.com>
2014-07-23 09:22:48 -0700

    added TimerTest to evaluate if it can be used for the game loop

---
+++ src/test/java/ch/tankwars/transport/game/timer/TimerTest.java
@@ -0,0 +1,30 @@
+package ch.tankwars.transport.game.timer;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.junit.Test;
+
+
+public class TimerTest {
+	
+	@Test
+	public void testAccuracy() throws Exception {
+		Timer timer = new Timer();
+		
+		timer.scheduleAtFixedRate(new TimerTask() {
+			long lastExecutionNano = System.nanoTime();
+			
+			
+			@Override
+			public void run() {
+				long executionTimeNanos = System.nanoTime() - lastExecutionNano;
+				lastExecutionNano = System.nanoTime();
+				System.out.println(executionTimeNanos / 1000000d);
+				
+			}
+		}, 0L, 20L);
+		
+		Thread.sleep(200);
+	}
+}
