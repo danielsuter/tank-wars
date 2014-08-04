09ad9dca599c8122f2e8469943690e6a566b1406
joachimprinzbach <joachim.prinzbach@gmail.com>
2014-08-04 12:19:48 -0700

    Added a pretty tank.

--- src/main/webapp/js/Game.js
+++ src/main/webapp/js/Game.js
@@ -112,13 +112,57 @@ var Game = function(canvasId) {
 
     var drawTank = function(tank) {
         var tankShape = tanks[tank.playerId];
+        var raupeLeft;
+        var raupeRight;
+        var tower;
+        var tankBody;
+        var pipe;
+
+
         if (!tankShape) {
-            tankShape = new fabric.Rect({
+
+            raupeLeft = new fabric.Rect({
                 left: tank.x,
                 top: tank.y,
-                fill: 'black',
+                fill: 'brown',
                 width: tank.width,
-                height: tank.height
+                height: 7
+            });
+
+            raupeRight = new fabric.Rect({
+                left: tank.x,
+                top: tank.y + 18,
+                fill: 'brown',
+                width: tank.width,
+                height: 7
+            });
+
+            tankBody = new fabric.Rect({
+                left: tank.x + 3,
+                top: tank.y + 4,
+                fill: 'black',
+                width: tank.width - 6,
+                height: tank.height - 8
+            });
+
+            tower = new fabric.Circle({
+                left: tank.x + 8,
+                top: tank.y + 8,
+                fill: 'brown',
+                radius: 5
+            });
+
+            pipe = new fabric.Rect({
+                left: tank.x + 12,
+                top: tank.y + 11,
+                fill: 'black',
+                width: tank.width - 12,
+                height: 4
+            });
+
+            tankShape = new fabric.Group([raupeLeft, raupeRight, tankBody, tower, pipe], {
+                left: tank.x,
+                top: tank.y
             });
 
             tanks[tank.playerId] = tankShape;
