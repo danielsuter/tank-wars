a56d23eb7af263193fc3ce6fa984f936faf2bb7c
joachimprinzbach <joachim.prinzbach@gmail.com>
2014-08-04 07:56:26 -0700

    Better ci detection based on correct ip.

--- src/main/webapp/js/util/TUtil.js
+++ src/main/webapp/js/util/TUtil.js
@@ -5,11 +5,11 @@ var TUtil = {};
 TUtil.getWebsocketLobbyUrl = function() {
     var wsUri;
     var hostname =  window.location.hostname;
-    if(hostname === 'localhost') {
-        wsUri = "ws://" + hostname + ":8080/tank-wars/lobby";
-    } else {
+    if(hostname === '91.138.8.56/') {
         wsUri = "ws://" + hostname + ":9090/tank-wars/lobby";
         console.log("detected CI, using wsUri: " + wsUri);
+    } else {
+        wsUri = "ws://" + hostname + ":8080/tank-wars/lobby";
     }
     return wsUri;
 };
@@ -19,11 +19,11 @@ TUtil.getWebsocketLobbyUrl = function() {
 TUtil.getWebsocketGameUrl = function() {
     var wsUri;
     var hostname =  window.location.hostname;
-    if(hostname === 'localhost') {
-        wsUri = "ws://" + hostname + ":8080/tank-wars/game";
-    } else {
+    if(hostname === '91.138.8.56/') {
         wsUri = "ws://" + hostname + ":9090/tank-wars/game";
         console.log("detected CI, using wsUri: " + wsUri);
+    } else {
+        wsUri = "ws://" + hostname + ":8080/tank-wars/game";
     }
     return wsUri;
 };
