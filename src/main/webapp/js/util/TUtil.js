5e523fe64cfc8ae199c9f00a43de071fd513239c
joachimprinzbach <joachim.prinzbach@gmail.com>
2014-08-04 07:57:46 -0700

    Better ci detection based on correct ip.

--- src/main/webapp/js/util/TUtil.js
+++ src/main/webapp/js/util/TUtil.js
@@ -5,7 +5,7 @@ var TUtil = {};
 TUtil.getWebsocketLobbyUrl = function() {
     var wsUri;
     var hostname =  window.location.hostname;
-    if(hostname === '91.138.8.56/') {
+    if(hostname === '91.138.8.56') {
         wsUri = "ws://" + hostname + ":9090/tank-wars/lobby";
         console.log("detected CI, using wsUri: " + wsUri);
     } else {
