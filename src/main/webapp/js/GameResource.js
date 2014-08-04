07816e91199751ac9d9e1d9a7e48573e516bafd0
nick <nick.payne89@gmail.com>
2014-08-04 03:13:15 -0700

    draw tanks based on actors

--- src/main/webapp/js/GameResource.js
+++ src/main/webapp/js/GameResource.js
@@ -1,9 +1,46 @@
 var GameResource = function(_onGameUpdate) {
     var onGameUpdate = _onGameUpdate;
 
-
     this.join = function(onJoined) {
-        onJoined(/* feld grösse, player id */);
+        onJoined(
+            {
+                width : 800,
+                height : 600
+            },
+            "abcdefghijklmnop"
+        );
+
+        onGameUpdate(
+            [
+                {
+                    playerName : "hugo",
+                    speed : 5,
+                    x : 10,
+                    y : 10,
+                    width : 10,
+                    height : 10
+                },
+                {
+                    playerName : "peter",
+                    speed : 5,
+                    x : 50,
+                    y : 50,
+                    width : 10,
+                    height : 10
+                },
+                {
+                    playerName : "fritz",
+                    speed : 5,
+                    x : 5,
+                    y : 150,
+                    width : 10,
+                    height : 10
+                }
+            ]
+        );
+
     };
+
+
 
 };
