332ddf9cf209cd7306216e3015606700316023e6
danielsuter <suter_daniel@hotmail.com>
2014-08-04 05:43:28 -0700

    improved logging

--- src/main/java/ch/tankwars/transport/game/GameController.java
+++ src/main/java/ch/tankwars/transport/game/GameController.java
@@ -10,21 +10,27 @@ import java.util.TimerTask;
 
 import javax.websocket.Session;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import ch.tankwars.game.Direction;
 import ch.tankwars.game.Game;
 import ch.tankwars.game.Tank;
 import ch.tankwars.transport.game.dto.JoinResponse;
 
 public class GameController {
-
+	private static final long INTERVAL_MILIS = 1000L;
+	
+	private final static Logger LOGGER = LoggerFactory.getLogger(GameController.class);
+	
 	private static Game game = new Game();
 	private static Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
-	private static final long INTERVAL_MILIS = 1000L;
 	private Timer timer;
 	private static GameCommunicator gameCommunicator = new GameCommunicator();
 	private final Map<Session, Tank> tanksMap = new HashMap<Session, Tank>();
 
 	public void start() {
+		LOGGER.info("game started");
 		timer = new Timer();
 		timer.scheduleAtFixedRate(new TimerTask() {
 
