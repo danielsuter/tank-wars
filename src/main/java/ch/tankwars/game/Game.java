bd95bee3a66699b2328b00048d2bea7aea4a5e73
danielsuter <suter_daniel@hotmail.com>
2014-08-04 08:38:16 -0700

    try to fix concurrent modification problem

--- src/main/java/ch/tankwars/game/Game.java
+++ src/main/java/ch/tankwars/game/Game.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * Contains all game logic.
@@ -14,9 +15,23 @@ public class Game implements ActorListener {
 	public static final int GAME_WIDTH = 800;
 	public static final int GAME_HEIGHT = 600;
 	
+	private ConcurrentLinkedQueue<Actor> actorsToAdd = new ConcurrentLinkedQueue<Actor>();
+	private ConcurrentLinkedQueue<Actor> actorsToRemove = new ConcurrentLinkedQueue<Actor>();
+	
+	
 	private List<Actor> actors = Collections.synchronizedList(new LinkedList<Actor>());
 	
 	public void tick() {
+		Actor actorToAdd = null;
+		while((actorToAdd = actorsToAdd.poll()) != null){
+			actors.add(actorToAdd);
+		}
+		
+		Actor actorToRemove = null;
+		while((actorToRemove = actorsToRemove.poll()) != null){
+			actors.remove(actorToRemove);
+		}
+		
 		for (Actor actor : actors) {
 			actor.act();
 		}
@@ -46,11 +61,11 @@ public class Game implements ActorListener {
 	}
 
 	public void remove(Tank tank) {
-		actors.remove(tank);
+		actorsToRemove.add(tank);
 	}
 
 	@Override
 	public void createActor(Actor actor) {
-		actors.add(actor);
+		actorsToAdd.add(actor);
 	}
 }
