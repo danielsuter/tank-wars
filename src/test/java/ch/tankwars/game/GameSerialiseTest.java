902546034dfecf7b3251df825ce13a44b4688c12
danielsuter <suter_daniel@hotmail.com>
2014-08-05 06:54:54 -0700

    extracted ActorListDeserializer

--- src/test/java/ch/tankwars/game/GameSerialiseTest.java
+++ src/test/java/ch/tankwars/game/GameSerialiseTest.java
@@ -9,7 +9,8 @@ import java.util.List;
 
 import org.junit.Test;
 
-import ch.tankwars.transport.game.GsonFactory;
+import ch.tankwars.transport.game.mapper.ActorListDeserializer;
+import ch.tankwars.transport.game.mapper.GsonFactory;
 import ch.tankwars.transport.game.mapper.ResponseMapper;
 
 import com.google.gson.Gson;
@@ -48,12 +49,12 @@ public class GameSerialiseTest {
 			tanks.stream().forEach(t -> t.shoot());
 			game.tick();
 			// Allow caching
-			mapper.map(game.getActors(), GsonFactory.ACTOR_LIST_TYPE);
+			mapper.map(game.getActors(), ActorListDeserializer.TYPE);
 		}
 		
 		game.tick();
 		List<Actor> actors = game.getActors();
-		String response = mapper.map(actors, GsonFactory.ACTOR_LIST_TYPE);
+		String response = mapper.map(actors, ActorListDeserializer.TYPE);
 		long actualSize = getSizeInBytes(response) * TICKS_PER_SECOND;
 		assertTrue("Expected size to be below " + MAX_SIZE_BYTES + " but was " + actualSize ,actualSize < MAX_SIZE_BYTES);
 	}
@@ -73,11 +74,11 @@ public class GameSerialiseTest {
 		tank.setPosition(0, 0);
 		tank.setVelocity(1);
 		tank.setDirection(Direction.DOWN);
-		mapper.map(game.getActors(), GsonFactory.ACTOR_LIST_TYPE);
+		mapper.map(game.getActors(), ActorListDeserializer.TYPE);
 		
 		game.tick();
 
-		String response2 = mapper.map(game.getActors(), GsonFactory.ACTOR_LIST_TYPE);
+		String response2 = mapper.map(game.getActors(), ActorListDeserializer.TYPE);
 		assertEquals("[0,{\"i\":1,\"y\":1}]", response2);
 		
 	}
