82071f441d66a6a6293e9dac5200faeb65f79e0a
joachimprinzbach <joachim.prinzbach@gmail.com>
2014-08-05 16:00:06 -0700

    Created own class for Dimensions.

---
+++ src/main/java/ch/tankwars/game/Dimensions.java
@@ -0,0 +1,69 @@
+package ch.tankwars.game;
+
+public class Dimensions {
+	
+	private int x;
+	private int y;
+	private int height;
+	private int width;
+	
+	public Dimensions(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public void setLocation(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void setBounds(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public void setSize(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
+	
+	
+
+}
