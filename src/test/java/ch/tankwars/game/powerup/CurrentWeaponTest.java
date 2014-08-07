2586a84832a36c3db8fc9ebc1f5b5e6997b452ff
danielsuter <suter_daniel@hotmail.com>
2014-08-07 08:05:08 -0700

    added test for canon change

---
+++ src/test/java/ch/tankwars/game/powerup/CurrentWeaponTest.java
@@ -0,0 +1,36 @@
+package ch.tankwars.game.powerup;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import ch.tankwars.game.Direction;
+import ch.tankwars.game.projectiles.Projectile;
+
+public class CurrentWeaponTest {
+
+	@Test
+	public void weaponChangeAfterXShots() {
+		CurrentWeapon currentWeapon = new CurrentWeapon();
+		currentWeapon.setWeapon(WeaponData.ROCKET_LAUNCHER);
+
+		for (int currentShot = 0; currentShot < WeaponData.ROCKET_LAUNCHER.getMaxShots(); currentShot++) {
+			Projectile shot = currentWeapon.shoot(0, Direction.LEFT);
+			assertEquals(WeaponData.ROCKET_LAUNCHER.getPower(), shot.getPower());
+		}
+		
+		Projectile shot = currentWeapon.shoot(0, Direction.LEFT);
+		assertEquals(WeaponData.STANDARD_CANON.getPower(), shot.getPower());
+		
+		currentWeapon.setWeapon(WeaponData.LASER_GUN);
+		for (int currentShot = 0; currentShot < WeaponData.LASER_GUN.getMaxShots(); currentShot++) {
+			shot = currentWeapon.shoot(0, Direction.LEFT);
+			assertEquals(WeaponData.LASER_GUN.getPower(), shot.getPower());
+		}
+		
+		shot = currentWeapon.shoot(0, Direction.LEFT);
+		assertEquals(WeaponData.STANDARD_CANON.getPower(), shot.getPower());
+	}
+	
+	
+}
