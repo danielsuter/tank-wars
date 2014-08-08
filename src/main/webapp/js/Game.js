af3e6280ca1fc51d3f0503405713918e59761649
danielsuter <suter_daniel@hotmail.com>
2014-08-08 06:59:42 -0700

    removed sound

--- src/main/webapp/js/Game.js
+++ src/main/webapp/js/Game.js
@@ -16,9 +16,6 @@ var Game = function(canvasId) {
      */
     var isDead = false;
 
-    var audioHit = AV.Player.fromURL(TUtil.getBaseUrl() + 'sound/hit.flac');
-
-
     var doKeyDown = function(event) {
         if(event.keyCode === lastCode || isDead) {
             return;
@@ -212,9 +209,6 @@ var Game = function(canvasId) {
         for (var id in knownActors) {
             if (ids.indexOf(parseInt(id)) === -1) {
                 renderer.removeShape(id);
-                if(knownActors[id].actorType === 'PROJECTILE') {
-                    audioHit.play();
-                }
                 delete knownActors[id];
             }
         }
