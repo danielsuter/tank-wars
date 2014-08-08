1fd37c582d5d300ac1a36302e9a8d18b7beadb19
danielsuter <suter_daniel@hotmail.com>
2014-08-08 06:55:13 -0700

    corrected url

--- src/main/webapp/js/util/TUtil.js
+++ src/main/webapp/js/util/TUtil.js
@@ -35,10 +35,10 @@ TUtil.getBaseUrl = function() {
     var wsUri;
     var hostname =  window.location.hostname;
     if(hostname === '91.138.8.56') {
-        wsUri = "ws://" + hostname + ":9090/tank-wars/";
+        wsUri = "http://" + hostname + ":9090/tank-wars/";
         console.log("detected CI, using wsUri: " + wsUri);
     } else {
-        wsUri = "ws://" + hostname + ":8080/tank-wars/";
+        wsUri = "http://" + hostname + ":8080/tank-wars/";
     }
     return wsUri;
 };
