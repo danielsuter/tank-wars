2c755fbd8e66d9006d3b0eaa9623cf87426c4289
nick <nick.payne89@gmail.com>
2014-08-08 17:07:10 +0200

    use img for walls

--- src/main/webapp/js/canvas/Mine.js
+++ src/main/webapp/js/canvas/Mine.js
@@ -2,12 +2,9 @@ var Mine = {
 
     drawMine: function (mine) {
 
-        var innerMine;
-        var trigger;
-        var imgSize = 256;
-
         var imgElement = document.getElementById('mineImg');
         var scaleFactor = mine.width / imgElement.width;
+
         var imgInstance = new fabric.Image(imgElement, {
             left: mine.x,
             top: mine.y,
